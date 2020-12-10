08ea045d80b98bd4511411dfc71339c59c24ab1e
Your Name <you@example.com>
2020-12-10 12:05:00 +0530

    updated code 10-12-2020

---
+++ build/precache-manifest.f5feca30b37990cbc8af35f0af7c2152.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72a57870bd5ff43b77d9c13b71d4e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b795799e3930dedcc417",
+    "url": "/static/css/2.128cbd5c.chunk.css"
+  },
+  {
+    "revision": "90a781c637010ccc4700",
+    "url": "/static/css/main.1f08bfff.chunk.css"
+  },
+  {
+    "revision": "b795799e3930dedcc417",
+    "url": "/static/js/2.697896ee.chunk.js"
+  },
+  {
+    "revision": "474891b8d41c72f508eccdb81d397211",
+    "url": "/static/js/2.697896ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a781c637010ccc4700",
+    "url": "/static/js/main.b03ae2d7.chunk.js"
+  },
+  {
+    "revision": "d3ef2486b38192aa22a3",
+    "url": "/static/js/runtime-main.8c3fa017.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bb37e735930f8e10403b65b49a4461e.js"
+  "/precache-manifest.f5feca30b37990cbc8af35f0af7c2152.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
